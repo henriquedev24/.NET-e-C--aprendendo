33937a3d777dad5347742374140fbcea8ef5f347
Gabriel Henrique <henriquedeveloper24@gmail.com>
2024-09-14 18:47:40 -0300

    learn

---
+++ ExemplosFundamentos.Common/Models/calculadora.cs
@@ -0,0 +1,63 @@
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace ExemplosFundamentos.Common.Models
+{
+    public class Calculadora
+    {
+        public void Somar(int x, int y)
+        {
+            Console.WriteLine($"{x} + {y} = {x + y}");
+        }
+
+        public void Subtrair(int x, int y)
+        {
+            Console.WriteLine($"{x} - {y} = {x - y}");
+        }
+
+        public void Multiplicar(int x, int y)
+        {
+            Console.WriteLine($"{x} x {y} = {x * y}");
+        }
+
+        public void Dividir(int x, int y)
+        {
+            Console.WriteLine($"{x} / {y} = {x / y}");
+        }
+
+        public void Potencia(int x, int y)
+        {
+            double pot = Math.Pow(x, y);
+            Console.WriteLine($"{x} ^ {y} = {pot}");
+        }
+
+        public void Seno(double angulo)
+        {
+            double radiano = angulo * Math.PI / 180;
+            double seno = Math.Sin(radiano);
+            Console.WriteLine($"Seno de {angulo} = {Math.Round(seno, 3)}");
+        }
+
+        public void Cosseno(double angulo)
+        {
+            double radiano = angulo * Math.PI / 180;
+            double cosseno = Math.Sin(radiano);
+            Console.WriteLine($"Cosseno de {angulo} = {Math.Round(cosseno, 4)}");
+        }
+
+        public void Tangente(double angulo)
+        {
+            double radiano = angulo * Math.PI / 180;
+            double tangente = Math.Sin(radiano);
+            Console.WriteLine($"Cosseno de {angulo} = {Math.Round(tangente, 3)}");
+        }
+
+        public void RaizQuadrada(double x)
+        {
+            double raiz = Math.Sqrt(x);
+            Console.WriteLine($"Raiz quadrada de {x} = {raiz}");
+        }
+    }
+}
---
+++ ExemplosFundamentos/Program.cs
@@ -0,0 +1,172 @@
+﻿using ExemplosFundamentos.Common.Models;
+
+List<string> listaString = new List<string>();
+
+
+listaString.Add("CE");
+listaString.Add("SP");
+listaString.Add("RJ");
+
+Console.WriteLine($"{listaString.Count} itens na minha lista e capacidade {listaString.Capacity}");
+
+listaString.Add("PE");
+
+Console.WriteLine($"{listaString.Count} itens na minha lista e capacidade {listaString.Capacity}");
+
+listaString.Remove("RJ");
+
+Console.WriteLine($"{listaString.Count} itens na minha lista e capacidade {listaString.Capacity}");
+
+// int[] arrayNumeros = new int[5];
+
+// arrayNumeros[0] = 15;
+// arrayNumeros[1] = 28;
+// arrayNumeros[2] = 9;
+// arrayNumeros[3] = 45;
+// arrayNumeros[4] = 82;
+
+// int[] arrayNumerosDobrados = new int[arrayNumeros.Length * 2];
+// Array.Copy(arrayNumeros, arrayNumerosDobrados, arrayNumeros.Length);
+
+// // Array.Resize(ref arrayNumeros, arrayNumeros.Length * 2);
+
+// int posicao = 0;
+// foreach (int numero in arrayNumeros)
+// {
+//     Console.WriteLine($"Posição {numero}");
+//     posicao++;
+// }
+
+
+// Pessoa pessoa1 = new Pessoa();
+// pessoa1.Nome = "Gabriel";
+// pessoa1.Idade = 20;
+// pessoa1.Apresentar();
+
+
+// int numero = 10;
+
+// for (int contador = 0; contador <= 10; contador++)
+// {
+//    Console.WriteLine($"{numero} x {contador} = {numero * contador}"); 
+// }
+// int contador = 0;
+// while (contador <= 10)
+// {
+//     Console.WriteLine($"{numero} x {contador} = {numero * contador}"); 
+//     contador++;
+
+//     if (contador == 5)
+//     {
+//         break;
+//     }
+// }
+
+
+// int soma = 0, numero = 0;
+
+// do
+// {
+//     Console.WriteLine("Digite um número(0 para parar)");
+//     numero = Convert.ToInt32(Console.ReadLine());
+    
+//     soma += numero;
+
+// } while (numero != 0);
+// Console.WriteLine($"Total da soma dos números é: {soma}");
+
+// DateTime dataAtual = DateTime.Now.AddDays(5);
+// Console.WriteLine(dataAtual);
+
+
+// string apresentacao = "Iae galera";
+// int quantidade = 5;
+// double altura = 1.50;
+
+// decimal preco = 1.70M;
+
+// bool condicao = true;
+
+// Console.WriteLine(apresentacao);
+// Console.WriteLine("Valor de quantidade: " + quantidade);
+// Console.WriteLine("Valor da altura: " + altura);
+// Console.WriteLine("Valor de double: " + preco);
+// Console.WriteLine("Valor de condicao: " + condicao);
+
+// Console.WriteLine("Digite uma letra:");
+// string letra = Console.ReadLine();
+
+// if (letra == "a" || letra == "e" || letra == "i" || letra == "o" || letra == "u")
+// {
+//     Console.WriteLine("Vogal");
+// }
+// else 
+// {
+//     Console.WriteLine("Não é vogal");
+// }
+
+// switch (letra)
+// {
+//     case "a":
+//     case "e":
+//     case "i":
+//     case "o":
+//     case "u":
+//         Console.WriteLine("Vogal");
+//         break;
+//     default: 
+//         Console.WriteLine("Não é vogal");
+//         break;
+// }
+
+// Pessoa pessoa1 = new Pessoa();
+
+// pessoa1.Nome = "Gabriel";
+// pessoa1.Idade = 20;
+// pessoa1.Apresentar();
+
+// Cast Casting
+// int a = Convert.ToInt32(null);
+// int quantidadeEmEstoque = 10;
+// int quantidadeCompra = 4;
+// bool possivelVenda = quantidadeEmEstoque >= quantidadeCompra;
+
+// if (possivelVenda)
+// {
+//     Console.WriteLine("Venda feita.");
+// }
+// else
+// {
+//     Console.WriteLine("Não temos esse tanto no estoque.");
+// }
+
+// Console.WriteLine($"Quantidade em estoque: {quantidadeEmEstoque}");
+// Console.WriteLine($"Quantidade de compra: {quantidadeCompra}");
+// Console.WriteLine($"É possível realizar a venda? {possivelVenda}");
+
+// Calculadora calc = new Calculadora();
+
+// calc.Somar(15, 30);
+// calc.Subtrair(15, 30);
+// calc.Multiplicar(15, 30);
+// calc.Dividir(15, 30);
+// calc.Potencia(3, 4);
+// calc.Seno(90);
+// calc.Cosseno(90);
+// calc.Tangente(90);
+// calc.RaizQuadrada(36);
+
+// int num = 10;
+
+// Console.WriteLine(num);
+
+// Console.WriteLine("Incrementando 10");
+// num++;
+// Console.WriteLine(num);
+
+// int num2 = 50;
+// Console.WriteLine("Retrocendendo 50");
+// num2--;
+
+// Console.WriteLine(num2);
+
